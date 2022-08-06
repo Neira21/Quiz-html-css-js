e80210283816da79b110c17eff2b07f574465293
Alvaro Neira <alvaroneira98@gmail.com>
2022-08-06 11:41:23 -0500

    kirby.png

--- js/question.js
+++ js/question.js
@@ -20,7 +20,7 @@ const quiz = [
         q:'¿Quien es este personaje?',
         options:['Kirby', 'Sans', 'Sonic'],
         answer: 0,
-        img:'../img/kirby.png'
+        img:'img/kirby.png'
     },
     {
         q:'¿Cuál de estos pokemon es de tipo fantasma/veneno?',
